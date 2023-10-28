64573d53cadde3b601f8a94d7731486f8eebb54d
oreoezi <oreoezi@oreo.ac>
2023-10-28 21:27:45 +0300

    Changed main.cpp network topology to the one in Networking lab 3

--- main.cpp
+++ main.cpp
@@ -18,13 +18,18 @@ int main() {
     brasov.setIpAddress(SubnetAddress(IPv4Address{"173.32.32.81"}, 21));
     brasov.setDefaultGateway(IPv4Address{"173.32.32.1"});
     brasov.connect(&sw_brasov.getNetworkAdapter()[0]);
+    brasov.setHostname("Brasov");
 
     sw_brasov.getNetworkAdapter()[1].connect(&r_brasov.getNetworkAdapter()[0]);
+    sw_brasov.setHostname("SW-BRASOV");
     r_brasov.getNetworkAdapter()[0].setIpAddress(SubnetAddress(IPv4Address{"173.32.32.1"}, 21));
-    brasov.sendARPRequest(brasov.getDefaultGateway());
+    r_brasov.setHostname("R-BRASOV");
 
     r_brasov.getNetworkAdapter()[1].setIpAddress(SubnetAddress(IPv4Address{"10.10.10.5"}, 30));
     r_bon.getNetworkAdapter()[1].setIpAddress(SubnetAddress(IPv4Address{"10.10.10.6"}, 30));
+    r_bon.setHostname("R-BON");
+
+    r_brasov.getNetworkAdapter()[1].connect(&r_bon.getNetworkAdapter()[1]);
 
     r_brasov.addStaticRoute(SubnetAddress(IPv4Address{"188.26.2.0"}, 24), IPv4Address{"10.10.10.6"});
     r_bon.addStaticRoute(SubnetAddress(IPv4Address{"173.32.32.0"}, 21), IPv4Address{"10.10.10.5"});
@@ -32,9 +37,14 @@ int main() {
     bon.setIpAddress(SubnetAddress(IPv4Address{"188.26.2.35"}, 24));
     bon.setDefaultGateway(SubnetAddress(IPv4Address{"188.26.2.1"}, 24));
     bon.connect(&sw_bon.getNetworkAdapter()[0]);
+    bon.setHostname("Bon");
 
     sw_bon.getNetworkAdapter()[1].connect(&r_bon.getNetworkAdapter()[0]);
+    sw_bon.setHostname("SW-BON");
     r_bon.getNetworkAdapter()[0].setIpAddress(SubnetAddress(IPv4Address{"188.26.2.1"}, 24));
-    bon.sendARPRequest(bon.getDefaultGateway());
+
+    L2Payload pl;
+    cout << "Bon sent data to Brasov.\r\n";
+    bon.sendData(pl, brasov.getAddress());
     return 0;
 }
