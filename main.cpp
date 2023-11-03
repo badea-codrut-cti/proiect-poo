c5288cb144bafe0c576257cff42400d8994d9cf2
oreoezi <oreoezi@oreo.ac>
2023-11-03 19:59:41 +0200

    Testing html files being embedded into binary

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <cstring>
 #include <iostream>
 #include "./ext/webview/webview.h"
+#include "./build/index.html.h"
+#include <string>
 
 using namespace std;
 
@@ -8,7 +10,8 @@ int main() {
     webview_t w = webview_create(0, NULL);
     webview_set_title(w, "Basic Example");
     webview_set_size(w, 480, 320, WEBVIEW_HINT_NONE);
-    webview_set_html(w, "Thanks for using webview!");
+    string content(reinterpret_cast<char*>(assets_index_html), assets_index_html_len);
+    webview_set_html(w, content.c_str());
     webview_run(w);
     webview_destroy(w);
     return 0;
