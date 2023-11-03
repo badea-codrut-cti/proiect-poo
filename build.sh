6e71fd219454ca2c567f5e9f5b71ae22af0dba1e
oreoezi <oreoezi@oreo.ac>
2023-11-03 16:05:16 +0200

    Initial Webview test

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+for i in "src/" "examples/"
+do
+  cppcheck --enable=all --std=c++11 --check-config --suppress=missingIncludeSystem $i
+done
+
 if [ -d "build" ]; then
   rm -rf build
 fi
@@ -9,24 +14,4 @@ cd build
 cmake ..
 cmake --build . --config Debug
 
-ASAN_OPTIONS=detect_leaks=1 ./autodragan
-
-cd ..
-
-for i in "src/" "examples/"
-do
-  cppcheck --enable=all --std=c++11 --check-config --suppress=missingIncludeSystem $i
-done
-
-cd build
-cmake ..
-cmake --build . --config Debug
-
-MSAN_OPTIONS=halt_on_error=1 ./autodragan
-
-cd ..
-valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes ./build/autodragan
-
-echo "==========================================="
-
-./build/autodragan
+./autodragan
--- main.cpp
+++ main.cpp
@@ -1,49 +1,15 @@
 #include <cstring>
 #include <iostream>
-#include "src/device/device.h"
-#include "src/device/devices/l2switch.h"
-#include "src/date/subnetaddress.h"
-#include "src/date/osi/network.h"
-#include "src/protocoale/arp.h"
-#include "src/protocoale/payload.h"
-#include "src/device/devices/router.h"
-#include "src/device/devices/end_device.h"
+#include "./ext/webview/webview.h"
 
 using namespace std;
 
 int main() {
-    EndDevice brasov, bon;
-    L2Switch sw_brasov, sw_bon;
-    Router r_brasov, r_bon;
-    brasov.setIpAddress(SubnetAddress(IPv4Address{"173.32.32.81"}, 21));
-    brasov.setDefaultGateway(IPv4Address{"173.32.32.1"});
-    brasov.connect(&sw_brasov.getNetworkAdapter()[0]);
-    brasov.setHostname("Brasov");
-
-    sw_brasov.getNetworkAdapter()[1].connect(&r_brasov.getNetworkAdapter()[0]);
-    sw_brasov.setHostname("SW-BRASOV");
-    r_brasov.getNetworkAdapter()[0].setIpAddress(SubnetAddress(IPv4Address{"173.32.32.1"}, 21));
-    r_brasov.setHostname("R-BRASOV");
-
-    r_brasov.getNetworkAdapter()[1].setIpAddress(SubnetAddress(IPv4Address{"10.10.10.5"}, 30));
-    r_bon.getNetworkAdapter()[1].setIpAddress(SubnetAddress(IPv4Address{"10.10.10.6"}, 30));
-    r_bon.setHostname("R-BON");
-
-    r_brasov.getNetworkAdapter()[1].connect(&r_bon.getNetworkAdapter()[1]);
-
-    r_brasov.addStaticRoute(SubnetAddress(IPv4Address{"188.26.2.0"}, 24), IPv4Address{"10.10.10.6"});
-    r_bon.addStaticRoute(SubnetAddress(IPv4Address{"173.32.32.0"}, 21), IPv4Address{"10.10.10.5"});
-
-    bon.setIpAddress(SubnetAddress(IPv4Address{"188.26.2.35"}, 24));
-    bon.setDefaultGateway(SubnetAddress(IPv4Address{"188.26.2.1"}, 24));
-    bon.connect(&sw_bon.getNetworkAdapter()[0]);
-    bon.setHostname("Bon");
-
-    sw_bon.getNetworkAdapter()[1].connect(&r_bon.getNetworkAdapter()[0]);
-    sw_bon.setHostname("SW-BON");
-    r_bon.getNetworkAdapter()[0].setIpAddress(SubnetAddress(IPv4Address{"188.26.2.1"}, 24));
-
-    L2Payload pl;
-    bon.sendData(pl, brasov.getAddress());
+    webview_t w = webview_create(0, NULL);
+    webview_set_title(w, "Basic Example");
+    webview_set_size(w, 480, 320, WEBVIEW_HINT_NONE);
+    webview_set_html(w, "Thanks for using webview!");
+    webview_run(w);
+    webview_destroy(w);
     return 0;
 }
