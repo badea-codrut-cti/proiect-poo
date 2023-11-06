1842c11320345fe4968fd8c5861c5485af4d35a5
oreoezi <oreoezi@oreo.ac>
2023-11-06 23:27:00 +0200

    Changed Windows CI workflow to install Webview2 from chocolatey

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 using namespace std;
 
 int main() {
-    webview_t w = webview_create(0, NULL);
-    webview_set_title(w, "Basic Example");
+    webview_t w = webview_create(0, nullptr);
+    webview_set_title(w, PROJECT_NAME);
     webview_set_size(w, 480, 320, WEBVIEW_HINT_NONE);
     string content(reinterpret_cast<char*>(assets_index_html), assets_index_html_len);
     webview_set_html(w, content.c_str());
