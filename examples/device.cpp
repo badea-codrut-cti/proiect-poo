45d2d5151b4862a3c63c9e2917c68c3a59953b33
oreoezi <oreoezi@oreo.ac>
2023-11-13 14:14:42 +0200

    Modified examples

--- examples/device.cpp
+++ examples/device.cpp
@@ -1,22 +1,22 @@
 #include "../src/device/devices/end_device.h"
 #include <cassert>
+#include <iostream>
 
 void arp() {
     EndDevice a, b;
-    a.connect(&b.getNetworkAdapter()[0]);
+    a.connect(&(EthernetInterface&)b);
 
     a.setIpAddress(SubnetAddress(IPv4Address("192.168.1.2"), 24));
     b.setIpAddress(SubnetAddress(IPv4Address("192.168.1.3"), 24));
+}
 
-    std::string new_mac = "01:02:03:04:05:06";
-    a.turnOff();
-    a.getNetworkAdapter()[0].setMacAddress(MACAddress{new_mac});
-    // Won't work if device is turned off
-    assert(a.getNetworkAdapter()[0].getMacAddress() != MACAddress{new_mac});
+void clone() {
+    EndDevice a;
+    a.setHostname("Bobita");
+    a.setDefaultGateway(IPv4Address("192.168.1.1"));
 
-    a.turnOn();
-    a.getNetworkAdapter()[0].setMacAddress(MACAddress{new_mac});
-    assert(a.getNetworkAdapter()[0].getMacAddress() == MACAddress{new_mac});
-
-    a.sendARPRequest(IPv4Address("192.168.1.3"), false);
+    auto* b = dynamic_cast<EndDevice*>(a.clone());
+    assert(b->getDefaultGateway() == a.getDefaultGateway()); 
+    assert(b->getHostname() == a.getHostname());
+    delete b;
 }
--- examples/main.cpp
+++ examples/main.cpp
@@ -1,8 +1,9 @@
 #include "address.h"
+#include "device.h"
 
 int main() {
     test_macaddress();
     test_ipv4();
-
+    clone();
     return 0;
 }
