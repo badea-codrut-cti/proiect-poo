b6343f93cb110962c953280ed2e28772c52ed363
oreoezi <oreoezi@oreo.ac>
2023-11-14 22:13:23 +0200

    Readded Lab 3 topology example

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,10 +1,12 @@
 #include "address.h"
 #include "device.h"
+#include "./topologies/labs.h"
 
 int main() {
     test_macaddress();
     test_ipv4();
     arp();
     clone();
+    labs();
     return 0;
 }
---
+++ examples/topologies/labs.cpp
@@ -0,0 +1,5 @@
+#include "lab3.h"
+
+void labs() {
+    lab3();
+}
